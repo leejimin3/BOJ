cfcd3bae81e8df68260b3aee6bda5d2e4d72bc30
Ji Min Lee <ttette1559@naver.com>
2022-10-19 21:15:04 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int a, b;
+	std::cin >> a >> b;
+
+	std::cout << a + b << "\n" << a - b << "\n" << a * b << "\n" << a / b << "\n" << a % b;
+
+
+
+	return 0;
+}
