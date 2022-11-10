d8f8c451442f84b20921fd827e44547cb0eba1ee
Ji Min Lee <ttette1559@naver.com>
2022-11-10 16:17:00 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 96 ms, Memory: 7560 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+
+#define pp pair<int, int>
+
+using namespace std;
+
+
+
+bool val(const pp& left, const pp& right)
+{
+	if(left.second == right.second)
+		return left.first < right.first;
+	else
+		return left.second < right.second;
+}
+
+int main(int argc, char* argv[])
+{
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+	int N;
+	int x, y;
+	cin >> N;
+	
+	multimap<int, int> map;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x >> y;
+		map.insert({x,y});
+	}
+
+	vector<pp> vec(map.begin(), map.end());
+	sort(vec.begin(), vec.end(), val);
+
+	for (auto i : vec)
+	{
+		cout << i.first << " " << i.second << "\n";
+	}
+
+	return 0;
+}
