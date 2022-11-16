ef91dd4c907c8b8563964c13972aaf6dd8f15275
Ji Min Lee <ttette1559@naver.com>
2022-11-16 23:49:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}
