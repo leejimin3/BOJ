3a3fd619bf8737b729045dda91d1ecd0dde7e2fe
Ji Min Lee <ttette1559@naver.com>
2022-11-26 12:33:03 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int tmp, sum = 0;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> tmp;
+		if(tmp < 40)
+			tmp = 40;
+		sum += tmp;
+	}
+	
+
+	cout << sum/5 << "\n";
+}
