0345e1c894ac3641b83d99b7a13a7ffdb442e604
Ji Min Lee <ttette1559@naver.com>
2022-11-27 22:27:58 +0900

    [Bronze IV] Title: 시험 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	int tmp, sum1=0, sum2 = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> tmp;
+		sum1 += tmp;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> tmp;
+		sum2 += tmp;
+	}
+
+	cout << max(sum1, sum2);
+
+}
