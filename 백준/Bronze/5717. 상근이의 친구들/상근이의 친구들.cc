96e14b281c950e96a474b4c17845970254ae033f
Ji Min Lee <ttette1559@naver.com>
2022-12-01 23:34:19 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	while (1)
+	{
+		int M, F;
+		cin >> M >> F;
+		if(M == 0 && F == 0)
+			break;
+		cout << M+F << "\n";
+
+	}
+
+}
