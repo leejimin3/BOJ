e86620ec00568fce35cc4286bc9fe38f4334f28f
Ji Min Lee <ttette1559@naver.com>
2022-12-09 23:14:41 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	int N, K, tmp = 0;
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < K; j++)
+		{
+			cin >> tmp;
+		}
+	}
+
+	cout << "비와이";
+	return 0;
+}
