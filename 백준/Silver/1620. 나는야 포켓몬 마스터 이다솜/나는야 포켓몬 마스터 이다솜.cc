ba2b4c0667e1eeb8aac6524f50eafd2c904119d8
leejimin3 <ttette1559@naver.com>
2023-03-07 01:25:19 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 180 ms, Memory: 28040 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -7,19 +7,14 @@ using namespace std;
 string list[100001];
 map<string, int> Map;
 
-void Add(int num)
+void Add(string name, int num)
 {
-	string name;
-	cin >> name;
-	
 	list[num] = name;
 	Map.insert({ name, num });
 }
 
-string Find()
+string Find(string name)
 {
-	string name;
-	cin >> name;
 
 	if(Map.find(name) != Map.end()) { return to_string(Map.find(name)->second); }
 	else { return list[stoi(name)];}
@@ -31,13 +26,21 @@ int main(int argc, char* argv[]) {
 	ios_base::sync_with_stdio(false);
 
 	int M, N;
-
+	string name;
 	cin >> M >> N;
 
 	vector<string> ans;
 
-	for (int i = 1; i <= M; i++) { Add(i); }
-	for (int i = 0; i < N; i++) { ans.emplace_back(Find()); }
+	for (int i = 1; i <= M; i++) 
+	{
+		cin >> name;
+		Add(name, i); 
+	}
+	for (int i = 0; i < N; i++) 
+	{ 
+		cin >> name;
+		ans.emplace_back(Find(name)); 
+	}
 
 	for (auto var : ans) { cout << var << "\n"; }
 
