02d886b77f30697d564b660a9e481de4d3cd0503
leejimin3 <ttette1559@naver.com>
2024-06-18 12:33:29 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,45 +1,31 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <algorithm>
 
-
-#define pp pair<int, int>
-
 using namespace std;
-
-
-
-bool val(const pp& left, const pp& right)
-{
-	if(left.first == right.first)
-		return left.second < right.second;
-	else
-		return left.first < right.first;
-}
 
 int main(int argc, char* argv[])
 {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	int N;
-	int x, y;
 	cin >> N;
-	
-	multimap<int, int> map;
+	int x, y = 0;
+	vector<pair<int, int>> vec;
+
+	int tmp;
 
 	for (int i = 0; i < N; i++)
 	{
 		cin >> x >> y;
-		map.insert({x,y});
+		vec.emplace_back(x, y);
 	}
 
-	vector<pp> vec(map.begin(), map.end());
-	sort(vec.begin(), vec.end(), val);
+	sort(vec.begin(), vec.end());
 
-	for (auto i : vec)
+	for (int i = 0; i < vec.size(); i++)
 	{
-		cout << i.first << " " << i.second << "\n";
+		cout << vec[i].first << " " << vec[i].second << "\n";
 	}
 
 	return 0;
