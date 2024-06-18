8270952f77bd0c9492ba73e6b5ed97cf0bcd8cec
leejimin3 <ttette1559@naver.com>
2024-06-18 12:03:37 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	if (N % 2 == 1) cout << "SK" << "\n";
+	else cout << "CY" << "\n";
+
+	return 0;
+}
