ae76437bea3a2c4dbdf724d34c94aea842f23c2e
leejimin3 <ttette1559@naver.com>
2024-07-25 21:53:57 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int N, M;
+int visit[9];
+int arr[9];
+
+void solution(int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < cnt; i++)
+		{
+			cout << arr[i] << " ";
+		}
+
+		cout << "\n";
+	}
+	else
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			if (visit[i] < M)
+			{
+				visit[i]++;
+				arr[cnt] = i;
+				solution(cnt + 1);
+				visit[i]--;
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M;
+	solution(0);
+
+	return 0;
+}
