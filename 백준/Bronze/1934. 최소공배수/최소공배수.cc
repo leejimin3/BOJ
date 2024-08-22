6c24725d6616aaf2fda6570f00b547e74289a96d
leejimin3 <ttette1559@naver.com>
2024-08-23 04:59:53 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int T, A, B;
+
+int solution(int A, int B)
+{
+	int r = A % B;
+	if (r == 0)
+	{
+		return B;
+	}
+	else
+	{
+		return solution(B, r);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> A >> B;
+
+		if (A < B)
+		{
+			cout << (A * B) / solution(A, B) << "\n";
+		}
+		else
+		{
+			cout << (A * B) / solution(B, A) << "\n";
+		}
+	}
+
+	return 0;
+} 
