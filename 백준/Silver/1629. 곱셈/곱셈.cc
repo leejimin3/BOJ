527fb1001371dd9522b7965f49cbb806b3e60464
leejimin3 <ttette1559@naver.com>
2024-08-31 20:59:12 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -22,7 +22,7 @@ ll Func(ll a, ll b, ll c)
 	// 귀납법 1 : A의 1승을 계산할 수 있다.(Base condition)
 	if (b == 1) return a % c;
 
-	// 귀납법 2 : A의 N승을 계산하면 AN및 AN+1승의 결과를 예할 수 있다.
+	// 귀납법 2 : A의 N승을 계산하면 2N승 및 2N+1승도 O(1)로 알 수 있다.
 	ll tmp = Func(a, b / 2, c);
 	tmp = tmp * tmp % c;
 
